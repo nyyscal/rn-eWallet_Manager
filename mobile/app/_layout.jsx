0932f0971003ec64694762ef23a0c26c5e907657
Nischal <nyyscal541556@gmail.com>
2025-05-28 17:16:10 +0545

    Routes Creation + Clerk Setup

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,9 +1,13 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { ClerkProvider} from "@clerk/clerk-expo"
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import SafeScreen from "../components/SafeScreen"
 export default function RootLayout() {
   return (
+    <ClerkProvider tokenCache={tokenCache}>
     <SafeScreen>
-      <Stack screenOptions={{headerShown:false}}/>
+      <Slot/>
     </SafeScreen>
+    </ClerkProvider>
 );
 }
